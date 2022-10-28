990c9e1ac041abc424c55315e529d5a7620341cc
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-10-28 15:09:50 +0700

    borrowing test

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 fn main() {
-    let x = String::from("Hello Rust!");
+    let mut x = String::from("Hello World!");
+
+    change_str(&mut x);
+
     println!("{}", x);
+    println!("{}", &x);
+    println!("{}", &mut x);
 
     let add = |a: i32, b: i32| a + b;
 
     let mut result = 0;
 
-    for x in 1..10 {
-        println!("{}", x);
+    for x in 1..5 {
+        print!("{}", x);
         result = add(result, x);
     }
 
+    println!();
+
     println!("result = {}", result)
+}
+
+pub fn change_str(str: &mut String) {
+    *str = (*str).replace("World", "Rust").to_string();
 }
